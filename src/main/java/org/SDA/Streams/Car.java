84ed9f6381436e259c91a9428546089f0ea9b0dc
Mateusz Zawistowski <maturasisko@gmail.com>
2022-10-16 13:59:18 +0200

    Added more functional examples

--- src/main/java/org/SDA/Streams/Car.java
+++ src/main/java/org/SDA/Streams/Car.java
@@ -21,6 +21,10 @@ public class Car {
         return engineType;
     }
 
+    public Integer getPrice() {
+        return price;
+    }
+
     public enum EngineType {
         Gasoline,
         Diesel,
@@ -32,13 +36,20 @@ public class Car {
     private final String model;
     private final Integer yearProduced;
     private final Integer horsePower;
+    private final Integer price;
     private final EngineType engineType;
 
-    public Car(String brand, String model, Integer yearProduced, Integer horsePower, EngineType engineType) {
+    public Car(String brand, String model, Integer yearProduced, Integer horsePower, Integer price, EngineType engineType) {
         this.brand = brand;
         this.model = model;
         this.yearProduced = yearProduced;
         this.horsePower = horsePower;
+        this.price = price;
         this.engineType = engineType;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s %d %d %d %s", brand, model, yearProduced, horsePower, price, engineType.toString());
     }
 }
--- src/main/java/org/SDA/Streams/CarDatabase.java
+++ src/main/java/org/SDA/Streams/CarDatabase.java
@@ -1,7 +1,6 @@
 package org.SDA.Streams;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class CarDatabase {
@@ -10,15 +9,6 @@ public class CarDatabase {
     public CarDatabase(Set<Car> cars) {
         this.cars = cars;
     }
-
-    // 5. Get all cars from given, given engine type and given brand
-
-    // 6. Get cheapest car
-    // 7. Get most expensive car from given year
-    // 8. Get cheapest car with given engine type and given brand
-
-    // 9. Get average price of a car from given year
-    // 10. Get maximum horsepower for each engine type of all cars from given year
 
     // 1. Get all cars
     public Set<Car> getAllCars() {
@@ -69,5 +59,83 @@ public class CarDatabase {
         // .(car)-pojedynczy element (zwraca wartość true/false).może dodać kolejną funkcję
         // lub zwrócić ponownie do kolekcji tak jak w tym przypadku
         return cars.stream().filter(car -> car.getBrand().equals(brand)).collect(Collectors.toSet());
+    }
+
+    // 5. Get all cars from given year, given engine type and given brand
+    public Set<Car> getAllCarsWithGivenYearEngineTypeAndBrand(Integer year, Car.EngineType engineType, String brand) {
+        return cars.stream()
+                .filter(car -> car.getYearProduced().equals(year))
+                .filter(car -> car.getBrand().equals(brand))
+                .filter(car -> car.getEngineType().equals(engineType))
+                .collect(Collectors.toSet());
+    }
+
+    // 6. Get cheapest car
+    public Optional<Car> getCheapestCar() {
+        return cars.stream().min(Comparator.comparing(Car::getPrice));
+    }
+
+    // 7. Get most expensive car from given year
+    public Optional<Car> getMostExpensiveCarFromGivenYear(Integer year) {
+        return cars.stream()
+                .filter(car -> car.getYearProduced().equals(year))
+                .max(Comparator.comparing(Car::getPrice));
+    }
+
+    // 8. Get most powerful car with given engine type and given brand
+    public Optional<Car> getMostPowerfulCarWithGivenEngineTypeAndBrand(Car.EngineType engineType, String brand) {
+        return cars.stream()
+                .filter(car -> car.getBrand().equals(brand))
+                .filter(car -> car.getEngineType().equals(engineType))
+                .max(Comparator.comparing(Car::getHorsePower));
+    }
+
+    // 9. Get average price of a car from given year
+    public Double getAveragePriceOfCarFromGivenYear(Integer year) {
+        return cars.stream()
+                .filter(car -> car.getYearProduced().equals(year))
+                .collect(Collectors.averagingInt(Car::getPrice));
+    }
+
+    public Map<Car.EngineType, Set<Car>> groupCarsPerEngineType() {
+        return Arrays.stream(Car.EngineType.values())
+                .collect(Collectors.toMap(engineType -> engineType,
+                        engineType -> cars.stream().filter(car -> car.getEngineType()
+                                .equals(engineType)).collect(Collectors.toSet())));
+    }
+
+    public static void example() {
+        HashSet<Car> cars = new HashSet<>();
+        cars.add(new Car("Mercedes", "W120", 1999, 100, 2000, Car.EngineType.Gasoline));
+        cars.add(new Car("Mercedes", "Super", 2000, 120, 8000, Car.EngineType.Gasoline));
+        cars.add(new Car("Mercedes", "AMG", 2012, 1000, 65000, Car.EngineType.Gasoline));
+        cars.add(new Car("Tesla", "S", 1970, 800, 700, Car.EngineType.Diesel));
+        cars.add(new Car("Tesla", "X", 2012, 70, 10000, Car.EngineType.Diesel));
+        cars.add(new Car("Volkswagen", "Passat", 2012, 150, 12000, Car.EngineType.Hybrid));
+        cars.add(new Car("Audi", "A8", 2090, 165, 2050, Car.EngineType.Hybrid));
+        CarDatabase db = new CarDatabase(cars);
+
+        db.getAllCars().forEach(System.out::println);
+        System.out.println();
+        db.getAllCarsFromYear(2012).forEach(System.out::println);
+        System.out.println();
+        db.getAllCarsWithGivenEngineType(Car.EngineType.Diesel).forEach(System.out::println);
+        System.out.println();
+        db.getAllCarsWithGivenBrand("Tesla").forEach(System.out::println);
+        System.out.println();
+        db.getAllCarsWithGivenYearEngineTypeAndBrand(2090, Car.EngineType.Hybrid, "Audi").forEach(System.out::println);
+        System.out.println();
+        db.getCheapestCar().ifPresent(System.out::println);
+        System.out.println();
+        db.getMostExpensiveCarFromGivenYear(2012).ifPresent(System.out::println);
+        System.out.println();
+        db.getMostPowerfulCarWithGivenEngineTypeAndBrand(Car.EngineType.Gasoline, "Mercedes").ifPresent(System.out::println);
+        System.out.println();
+
+        Map<Car.EngineType, Set<Car>> map = db.groupCarsPerEngineType();
+        for (Car.EngineType key : map.keySet()) {
+            System.out.println(key.toString());
+            map.get(key).forEach(System.out::println);
+        }
     }
 }
